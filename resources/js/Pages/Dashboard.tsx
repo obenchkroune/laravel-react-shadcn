cb369f08f9ece6fc5e6797e2b6179040e467dddd
OUSSAMA BENCHKRON <obenchkroune01@gmail.com>
2023-07-24 21:44:42 +0100

    first commit

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,10 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head, Link } from '@inertiajs/react';
+import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
-import { Card, CardContent, CardHeader, CardTitle } from '@/Components/ui/card';
-import { Button } from '@/Components/ui/button';
-import { ArrowRightCircle, PlusIcon } from 'lucide-react';
-import { Separator } from '@/Components/ui/separator';
+import { Card, CardContent } from '@/Components/ui/card';
 
 export default function Dashboard({ auth }: PageProps) {
     return (
@@ -12,26 +9,11 @@ export default function Dashboard({ auth }: PageProps) {
             <Head title='Dashboard' />
 
             <div className='container'>
-                <div className='flex justify-end mb-8'>
-                    <Button asChild>
-                        <Link href='#'>
-                            <PlusIcon className='h-5 w-5 me-2' />
-                            <span>Create new bill</span>
-                        </Link>
-                    </Button>
-                </div>
-
-                {/* Stats */}
-                <div className='grid sm:grid-cols-2 lg:grid-cols-3'>
-                    <Card>
-                        <CardHeader>
-                            <CardTitle>Total bills</CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            <p>5 bills</p>
-                        </CardContent>
-                    </Card>
-                </div>
+                <Card>
+                    <CardContent className='p-6'>
+                        <p>you are logged in!</p>
+                    </CardContent>
+                </Card>
             </div>
         </AuthenticatedLayout>
     );

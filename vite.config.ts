efb2c580aaedfea7e3d37e1a62c69c2c1d8f0354
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2024-11-27 11:16:45 +0100

    add TypeScript transformer configuration and watch plugin for automatic updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { watch } from 'vite-plugin-watch';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: "inertia/app.tsx",
-            ssr: "inertia/ssr.tsx",
-            refresh: true,
-        }),
-        react(),
-    ],
-    resolve: {
-        alias: {
-            "~": path.resolve(__dirname, "./inertia"),
-        },
+  plugins: [
+    watch({
+      pattern: 'app/{Data,Enums}/**/*.php',
+      command: 'php artisan typescript:transform',
+    }),
+    laravel({
+      input: 'inertia/app.tsx',
+      ssr: 'inertia/ssr.tsx',
+      refresh: true,
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './inertia'),
     },
+  },
 });
